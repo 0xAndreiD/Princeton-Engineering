7c497530ed976f1d6525155fc004c0c3eb7ff7e5
MrCrocodileDev <luckycrocodile2261@gmail.com>
2020-12-08 21:44:12 +0900

    fix: remove crafted by on admin footer

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -244,7 +244,7 @@
                 <div class="content py-0">
                     <div class="row font-size-sm">
                         <div class="col-sm-6 order-sm-2 mb-1 mb-sm-0 text-center text-sm-right">
-                            Crafted with <i class="fa fa-heart text-danger"></i> by <a class="font-w600" href="#" target="_blank">.</a>
+                            .
                         </div>
                         <div class="col-sm-6 order-sm-1 text-center text-sm-left">
                             <a class="font-w600" href="#" target="_blank">Copyright © 2020 Richard Pantel. All Rights Reserved</span>
