6951ba719d75a6c823571b59ddb717da8d5ed0fa
MrCrocodileDev <luckycrocodile2261@gmail.com>
2020-12-18 02:34:24 +0900

    add: authentication to pings

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -38,20 +38,30 @@ class APIController extends Controller
      * @return JSON
      */
     public function getJobList(Request $request){
-        $query = new JobRequest;
-        if(isset($request['dateFrom']))
-            $query = $query->where('createdTime', '>=', $request['dateFrom']);
-        if(isset($request['dateTo']))
-        {
-            $dateTo = date('Y-m-d H:i:s',strtotime('+23 hour +59 minutes +59 seconds',strtotime($request['dateTo'])));
-            $query = $query->where('createdTime', '<=', $dateTo);
+        if(isset($request['username']) && isset($request['password'])){
+            $user = User::where('username', '=', $request['username'])->where('password', '=', $request['password'])->first();
+            if($user && $user->userrole == 2)
+            {
+                $query = new JobRequest;
+                if(isset($request['dateFrom']))
+                    $query = $query->where('createdTime', '>=', $request['dateFrom']);
+                if(isset($request['dateTo']))
+                {
+                    $dateTo = date('Y-m-d H:i:s',strtotime('+23 hour +59 minutes +59 seconds',strtotime($request['dateTo'])));
+                    $query = $query->where('createdTime', '<=', $dateTo);
+                }
+                if(isset($request['clientIdFrom']))
+                    $query = $query->where('companyId', '>=', $request['clientIdFrom']);
+                if(isset($request['clientIdTo']))
+                    $query = $query->where('companyId', '<=', $request['clientIdTo']);
+                
+                return response()->json(['success' => true, 'message' => 'Success', 'data' => $query->get()]);
+            }
+            else
+                return response()->json(['success' => false, 'message' => 'Auth failed.']);
         }
-        if(isset($request['clientIdFrom']))
-            $query = $query->where('companyId', '>=', $request['clientIdFrom']);
-        if(isset($request['clientIdTo']))
-            $query = $query->where('companyId', '<=', $request['clientIdTo']);
-        
-        return response()->json(['success' => true, 'message' => 'Success', 'data' => $query->get()]);
+        else
+            return response()->json(['success' => false, 'message' => 'Auth required.']);
     }
 
     /**
@@ -60,25 +70,35 @@ class APIController extends Controller
      * @return JSON
      */
     public function updateJobData(Request $request){
-        if(isset($request['requestFile']))
-        {
-            $job = JobRequest::where('requestFile', $request['requestFile'])->first();
-            if($job){
-                $columns = Schema::getColumnListing('job_request');
-                foreach($columns as $column){
-                    if($column != 'requestFile' && isset($request[$column]))
-                        $job[$column] = $request[$column];
+        if(isset($request['username']) && isset($request['password'])){
+            $user = User::where('username', '=', $request['username'])->where('password', '=', $request['password'])->first();
+            if($user && $user->userrole == 2)
+            {
+                if(isset($request['requestFile']))
+                {
+                    $job = JobRequest::where('requestFile', $request['requestFile'])->first();
+                    if($job){
+                        $columns = Schema::getColumnListing('job_request');
+                        foreach($columns as $column){
+                            if($column != 'requestFile' && isset($request[$column]))
+                                $job[$column] = $request[$column];
+                        }
+                        if($job->save())
+                            return response()->json(['success' => true, 'message' => 'Success']);
+                        else
+                            return response()->json(['success' => false, 'message' => 'Fail', 'reason' => 'Database Error.']);
+                    }
+                    else
+                        return response()->json(['success' => false, 'message' => 'Fail', 'reason' => 'Cannot find file.']);
                 }
-                if($job->save())
-                    return response()->json(['success' => true, 'message' => 'Success']);
                 else
-                    return response()->json(['success' => false, 'message' => 'Fail', 'reason' => 'Database Error.']);
+                    return response()->json(['success' => false, 'message' => 'Fail', 'reason' => 'Wrong File Name.']);
             }
             else
-                return response()->json(['success' => false, 'message' => 'Fail', 'reason' => 'Cannot find file.']);
+                return response()->json(['success' => false, 'message' => 'Auth failed.']);
         }
         else
-            return response()->json(['success' => false, 'message' => 'Fail', 'reason' => 'Wrong File Name.']);
+            return response()->json(['success' => false, 'message' => 'Auth required.']);
     }
 
     /**
@@ -87,13 +107,23 @@ class APIController extends Controller
      * @return JSON
      */
     public function downloadFile(Request $request){
-        if(isset($request['fileName'])){
-            if( Storage::disk('local')->exists($request['fileName']) )
-                return response()->download(storage_path('/app/' . $request['fileName']));
+        if(isset($request['username']) && isset($request['password'])){
+            $user = User::where('username', '=', $request['username'])->where('password', '=', $request['password'])->first();
+            if($user && $user->userrole == 2)
+            {
+                if(isset($request['fileName'])){
+                    if( Storage::disk('local')->exists($request['fileName']) )
+                        return response()->download(storage_path('/app/' . $request['fileName']));
+                    else
+                        return response()->json(['success' => false, 'message' => 'Fail', 'reason' => 'Cannot find file.']);
+                }
+                else
+                    return response()->json(['success' => false, 'message' => 'Fail', 'reason' => 'Wrong File Name.']);
+            }
             else
-                return response()->json(['success' => false, 'message' => 'Fail', 'reason' => 'Cannot find file.']);
+                return response()->json(['success' => false, 'message' => 'Auth failed.']);
         }
         else
-            return response()->json(['success' => false, 'message' => 'Fail', 'reason' => 'Wrong File Name.']);
+            return response()->json(['success' => false, 'message' => 'Auth required.']);
     }
 }
