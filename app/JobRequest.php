411fbb4e42b9c27e51e6ae16138e045b894358e0
MrCrocodileDev <luckycrocodile2261@gmail.com>
2021-01-23 00:43:04 +0900

    fix: wrong field collarHeights on JobRequest

--- app/JobRequest.php
+++ app/JobRequest.php
@@ -15,7 +15,7 @@ class JobRequest extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'id', 'companyName', 'companyId', 'userId', 'clientProjectName', 'clientProjectNumber', 'requestFile', 'planStatus', 'projectState', 'analysisType', 'createdTime', 'submittedTime', 'timesDownloaded', 'timesEmailed', 'timesComputed', 'collarHeights'
+        'id', 'companyName', 'companyId', 'userId', 'clientProjectName', 'clientProjectNumber', 'requestFile', 'planStatus', 'projectState', 'analysisType', 'createdTime', 'submittedTime', 'timesDownloaded', 'timesEmailed', 'timesComputed'
     ];
 
     protected $table = 'job_request';
