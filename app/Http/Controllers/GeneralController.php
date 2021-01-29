4fa6f70c17a8e403eb8b60158933225af508f96c
MrCrocodileDev <luckycrocodile2261@gmail.com>
2021-01-29 19:23:26 +0900

    fix: return est time on file upload

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -874,7 +874,7 @@ class GeneralController extends Controller
                 $dropboxFile = new DropboxFile($localpath);
                 $dropfile = $dropbox->upload($dropboxFile, env('DROPBOX_PREFIX') . $filepath . '/' . $file->getClientOriginalName(), ['autorename' => TRUE]);
                 
-                return response()->json(['success' => true, 'message' => 'Multiple Image File Has Been uploaded Successfully', 'filename' => $file->getClientOriginalName(), 'date' => $dropfile->getClientModified()]);
+                return response()->json(['success' => true, 'message' => 'Multiple Image File Has Been uploaded Successfully', 'filename' => $file->getClientOriginalName(), 'date' => date('Y-m-d H:i:s', strtotime('-5 hour',strtotime($dropfile->getClientModified())))]);
             } else {
                 return response()->json(['success' => false, 'message' => 'Cannot find the project.']);
             }
