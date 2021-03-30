943080cbc2b059f5aff8aa7518a1bda77bc3f8a6
MrCrocodileDev <luckycrocodile2261@gmail.com>
2021-03-30 09:16:22 +0900

    update: terms & policy

--- resources/views/clientadmin/layout.blade.php
+++ resources/views/clientadmin/layout.blade.php
@@ -287,7 +287,7 @@
                             .
                         </div>
                         <div class="col-sm-6 order-sm-1 text-center text-sm-left">
-                            <a class="font-w600" href="#" target="_blank">Copyright © 2020 Richard Pantel. All Rights Reserved</span>
+                            <span class="font-w600" >Copyright © 2020 Richard Pantel. All Rights Reserved</span>
                         </div>
                     </div>
                 </div>
@@ -309,20 +309,46 @@
                                 </button>
                             </div>
                         </div>
-                        <div class="block-content">
-                            <h5>1. Terms & conditions</h5>
-                            <p>Dolor posuere proin blandit accumsan senectus netus nullam curae, ornare laoreet adipiscing luctus mauris adipiscing pretium eget fermentum, tristique lobortis est ut metus lobortis tortor tincidunt himenaeos habitant quis dictumst proin odio sagittis purus mi, nec taciti vestibulum quis in sit varius lorem sit metus mi.</p>
+                        <div class="block-content termsDlg">
+                        <h5>1. Terms / Conditions / Privacy Policies</h5>
+                            <p>We may collect personal identification information from Users in a variety of ways, including, but not limited to, when Users visit our site, place an order and in connection with other activities, services, features or resources we make available on our Site. Users may be asked for, as appropriate, name, email address, mailing address, phone number, credit card information,</p>
+                            <p>Users may, however, visit the public portions of our Site anonymously.</p>
+                            <p>We will collect personal identification information from Users only if they voluntarily submit such information to us. Users can always refuse to supply personally identification information, except that it may prevent them from engaging in certain Site related activities.</p>
+                            <h6>Non-personal identification information</h6>
+                            <p>We may collect non-personal identification information about Users whenever they interact with our Site. Non-personal identification information may include the browser name, the type of computer and technical information about Users means of connection to our Site, such as the operating system and the Internet service providers utilized and other similar information.</p>
+                            <h6>Web browser cookies</h6>
+                            <p>Our Site may use "cookies" to enhance User experience. User's web browser places cookies on their hard drive for record-keeping purposes and sometimes to track information about them. User may choose to set their web browser to refuse cookies, or to alert you when cookies are being sent. If they do so, note that some parts of the Site may not function properly.</p>
+                            <h6>How we use collected information</h6>
+                            <p>TECTONICORP, P.C. dba Princeton Engineering,: collects and uses Users personal information for the following purposes:</p>
+                            <p><p style="font-weight: bold; margin-bottom: 0rem">To process transactions</p>
+                            We may use the information Users provide about themselves when placing an order only to provide service to that order. We do not share this information with outside parties except to the extent necessary to provide the service.
+                            </p>
+                            <p><p style="font-weight: bold; margin-bottom: 0rem">To send periodic emails</p>
+                            The email address Users provide for order processing, will only be used to send them information and updates pertaining to their order. It may also be used to respond to their inquiries, and/or other requests or questions. If User decides to opt-in to our mailing list, they will receive emails that may include company news, updates, related product or service information, etc. If at any time the User would like to unsubscribe from receiving future emails, we include detailed unsubscribe instructions at the bottom of each email or User may contact us via our Site.
+                            </p>
+                            <h6>How we protect your information</h6>
+                            <p>We adopt appropriate data collection, storage and processing practices and security measures to protect against unauthorized access, alteration, disclosure or destruction of your personal information, username, password, transaction information and data stored on our Site.</p>
+                            <p>Sensitive and private data exchange between the Site and its Users happens over a SSL secured communication channel and is encrypted and protected with digital signatures. </p>
+                            <h6>Third party websites</h6>
+                            <p>Users may find advertising or other content on our Site that link to the sites and services of our partners, suppliers, advertisers, sponsors, licensors and other third parties. We do not control the content or links that appear on these sites and are not responsible for the practices employed by websites linked to or from our Site. In addition, these sites or services, including their content and links, may be constantly changing. These sites and services may have their own privacy policies and customer service policies. Browsing and interaction on any other website, including websites which have a link to our Site, is subject to that website\'s own terms and policies.</p>
+                            <h6>Changes to this privacy policy</h6>
+                            <p><span style="font-weight: bold;">TECTONICORP, P.C. dba Princeton Engineering</span> has the discretion to update this privacy policy at any time. When we do, revise the updated date at the bottom of this page,. We encourage Users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy policy periodically and become aware of modifications.</p>
+                            <h6>Your acceptance of these terms</h6>
+                            <p>By using this Site, you signify your acceptance of this policy and terms of service. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.</p>
+                            <h6>Contacting us</h6>
+                            <p>If you have any questions about this Privacy Policy, the practices of this site, or your dealings with this site, please contact us at: info@Princeton-Engineering.com or you can call us at 908-507-5500.</p>
+                            
                             <h5>2. Copyright</h5>
-                            <p>Copyright © 2020 Richard Pantel.
-        All Rights Reserved.<span>&nbsp; </span>No parts of this
-        data input form or related calculation reports may be copied in format,
-        content or intent, or reproduced in any form or by any electronic or mechanical
-        means, including information storage and retrieval systems, without
-        permission in writing from the author.<span>&nbsp;
-        </span>Further, dis-assembly or reverse engineering of this data input form
-        or related calculation reports is strictly prohibited. The author's contact
-        information is: RPantel@Princeton-Engineering.com, web-site:
-        www.Princeton-Engineering.com; tel: 908-507-5500</p>
+                            <p>Copyright © 2020 Richard Pantel. All Rights Reserved.</p>
+                            <p>No parts of this data input form or related calculation reports may be copied in format,
+                            content or intent, or reproduced in any form or by any electronic or mechanical
+                            means, including information storage and retrieval systems, without
+                            permission in writing from the author.<span>&nbsp;
+                            </span>Further, disassembly or reverse engineering of this data input form
+                            or related calculation reports is strictly prohibited.</p>
+                            <p>The author's contact
+                            information is: RPantel@Princeton-Engineering.com, web-site:
+                            www.Princeton-Engineering.com; tel: 908-507-5500</p>
                         </div>
                         <div class="block-content block-content-full text-right bg-light">
                             <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal">Done</button>
