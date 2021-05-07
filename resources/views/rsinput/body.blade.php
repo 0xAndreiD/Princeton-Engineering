59535bfcc007d2b52cc3af62a0f3b3ba090ea801
MrCrocodileDev <luckycrocodile2261@gmail.com>
2021-05-07 17:38:59 +0900

    Obfuscate for 3 pdf forms

--- resources/views/rsinput/body.blade.php
+++ resources/views/rsinput/body.blade.php
@@ -81,6 +81,6 @@
 <script src="{{ asset('/js/plugins/pako.min.js') }}"></script>
 <script src="{{ asset('/js/plugins/pdfform.js') }}"></script>
 
-@include('rsinput.script_origin')
+@include('rsinput.script_obfuscate')
 
 @endsection
